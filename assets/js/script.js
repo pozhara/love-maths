9b2b8d5a29346280987aa0ae26535c08f9de86b5
pozhara <poliazhara04@gmail.com>
2022-10-20 14:44:15 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -26,10 +26,26 @@ function runGame(gameType){
     }
 }
 function checkAnswer(){
-
+    let userAnswer=parseInt(document.getElementById("answer-box").value);
+    let calculateAnswer=calculateCorrectAnswer();
+    let isCorrect=userAnswer===calculateAnswer[0];
+    if(isCorrect){
+        alert("Hey! You got it right!");
+    } else {
+        alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculateAnswer[0]}!`);
+    }
+    runGame(calculateAnswer[1]);
 }
 function calculateCorrectAnswer(){
-
+    let operand1=parseInt(document.getElementById("operand1").innerText);
+    let operand2=parseInt(document.getElementById("operand2").innerText);
+    let operator=document.getElementById("operator").innerText;
+    if(operator==="+"){
+        return[operand1+operand2, "addition"];
+    } else {
+        alert(`Uninmplemented operator ${operator}`);
+        throw `Uninmplemented operator ${operator}. Aborting!`;
+    }
 }
 function incrementScore(){
 
