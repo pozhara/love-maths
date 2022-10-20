930ae7cbba94f79e07f0f0a8a9832b0e1f352eef
pozhara <poliazhara04@gmail.com>
2022-10-20 12:21:04 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,29 @@
-function runGame(){
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+    runGame("addition");
+});
+
+function runGame(gameType){
+    let num1=Math.floor(Math.random()*25+1);
+    let num2=Math.floor(Math.random()*25+1);
+
+    if(gameType==="addition"){
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unkwown game type: ${gameType}. Aborting!`;
+    }
 }
 function checkAnswer(){
 
@@ -13,12 +37,14 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent=operand1;
+    document.getElementById("operand2").textContent=operand2;
+    document.getElementById("operator").textContent="+";
 }
 function displaySubstractQuestion(){
 
 }
 function displayMultiplyQuestion(){
-    
+
 }
